a9f9b819fb055b65508c49c36119992e9b861bae
Andres Pantoja <pantocode@gmail.com>
2023-03-03 21:00:33 -0600

    fix avatar img

--- portfolio/src/Pages/Home.jsx
+++ portfolio/src/Pages/Home.jsx
@@ -3,7 +3,7 @@ export function Home() {
   return (
     <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row">
-            <img src="../../public/Avatar.jpeg" className="max-w-sm rounded-lg shadow-2xl" />
+            <img src="https://imgur.com/3zgXQHN" className="max-w-sm rounded-lg shadow-2xl" />
             <div>
             <h1 className="text-5xl font-bold">Hi, I'm Andres Pantoja.</h1>
             <p className="py-6">Application Developer @IBM 👁️🐝Ⓜ️ I am currently focused on gaining expertise in Javascript, React, and NodeJS 💛</p>

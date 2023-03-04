3f577c469b7b4b786b79ebc46739ba0b58cf6f55
Andres Pantoja <pantocode@gmail.com>
2023-03-03 21:02:44 -0600

    fix photo

--- portfolio/src/Pages/Home.jsx
+++ portfolio/src/Pages/Home.jsx
@@ -3,7 +3,7 @@ export function Home() {
   return (
     <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row">
-            <img src="https://imgur.com/3zgXQHN" className="max-w-sm rounded-lg shadow-2xl" />
+            <img src="https://i.imgur.com/3zgXQHN.jpg" className="max-w-sm rounded-lg shadow-2xl"  alt="profile-photo"/>
             <div>
             <h1 className="text-5xl font-bold">Hi, I'm Andres Pantoja.</h1>
             <p className="py-6">Application Developer @IBM 👁️🐝Ⓜ️ I am currently focused on gaining expertise in Javascript, React, and NodeJS 💛</p>
